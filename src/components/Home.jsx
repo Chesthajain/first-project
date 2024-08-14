0ebbfe909af664c2fa2e1dc28818999426b71f35
Chesthajain <cheshtajain1712@gmail.com>
2024-08-14 20:41:33 +0530

    project

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <main>
+            <h1>TechnoStar</h1>
+            <p>Soltions that are mindBlowing</p>
+        </main>
+    </div>
+  )
+}
+
+export default Home;
