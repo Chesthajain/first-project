76f990997dcaed99cc0667c4596910695f52c316
Chesthajain <cheshtajain1712@gmail.com>
2024-08-22 20:18:52 +0530

    continue

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Services from "./components/Services";
 import Contact from "./components/Contact";
 import "./styles/App.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/footer.scss";
-import "./styles/contact.scss"
+import "./styles/contact.scss";
+import "./styles/mediaquery.com";
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
